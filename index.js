8b337d7ecdbedc859fec75053a4b040e6991e8d3
binhdao29 <binhchan0293@gmail.com>
2021-02-17 11:33:10 +0700

    Add users list

--- index.js
+++ index.js
@@ -4,7 +4,16 @@ var port = 3000;
 app.set('views', './views');
 app.set('view engine', 'pug');
 app.get('/', function(request, response) {
-    response.render('index');
+    response.render('index', {
+        users: [
+            { name: 'Tom'},
+            { name: 'Haley'},
+            { name: 'Alex'}
+        ]
+    });
+});
+app.get('/user', function(request, response) {
+    response.send('Users List');
 });
 app.listen(port, function() {
     console.log('Example express listening');
