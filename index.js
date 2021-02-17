ee2b704464d1d3a8f2c26587c844ddd366fbed10
binhdao29 <binhchan0293@gmail.com>
2021-02-17 11:13:31 +0700

    views folder template engine Pug

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
 var port = 3000;
+app.set('views', './views');
+app.set('view engine', 'pug');
 app.get('/', function(request, response) {
-    response.send('<h1>Hello everybody</h1>')
+    response.render('index');
 });
 app.listen(port, function() {
     console.log('Example express listening');
