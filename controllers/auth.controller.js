d7300a7bc8dbc3ad8570fd51fb29f74a92361568
binhdao29 <binhchan0293@gmail.com>
2021-02-20 23:58:14 +0700

    Example cookie and create form Login

---
+++ controllers/auth.controller.js
@@ -0,0 +1,35 @@
+var db = require('../db');
+const { use } = require('../routes/user.route');
+
+module.exports.login = function(request, response) {
+    response.render('auth/login', { users: db.get('users').value() });
+};
+
+module.exports.postLogin = function(req, res) {
+    var email = req.body.email;
+    var password = req.body.password;
+    var user = db.get('users').find({ email: email}).value();
+
+    if (!user) {
+        res.render('auth/login', {
+            errors: [
+                'User does not exit!'
+            ],
+            values: req.body
+        });
+        return;
+    }
+
+    if (user.password !== password) {
+        res.render('auth/login', {
+            errors: [
+                'Wrong password!'
+            ],
+            values: req.body
+        });
+        return;
+    }
+
+    res.cookie('userID', user.id);
+    res.redirect('/users');
+};
--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var db = require('./db.js');
+var cookieParser = require('cookie-parser');
+
+var authMiddleware = require('./middlewares/auth.middleware');
 
 var userRoute = require('./routes/user.route');
+var authRoute = require('./routes/auth.route');
 var port = 3000;
 var app = express();
 app.set('views', './views');
@@ -10,13 +14,15 @@ app.set('view engine', 'pug');
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
-app.use(express.static('public'))
+app.use(express.static('public'));
+app.use(cookieParser());
 
 app.get('/', function(request, response) {
     response.render('index', { users: db.get('users').value() });
 });
 
-app.use('/users',userRoute);
+app.use('/users', authMiddleware.requireAuth, userRoute);
+app.use('/auth', authRoute);
 
 app.listen(port, function() {
     console.log('Example express listening...');
