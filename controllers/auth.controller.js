a0e48beb5b39e5a6c7b6a06d2ee6e02d35bebc97
binhdao29 <binhchan0293@gmail.com>
2021-02-21 11:07:14 +0700

    Signed Cookie

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -32,6 +32,8 @@ module.exports.postLogin = function(req, res) {
         return;
     }
 
-    res.cookie('userID', user.id);
+    res.cookie('userID', user.id, {
+        signed: true
+    });
     res.redirect('/users');
 };
--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 app.use(express.static('public'));
-app.use(cookieParser());
+app.use(cookieParser('daocongbinhlearnnodejs'));
 
 app.get('/', function(request, response) {
     response.render('index', { users: db.get('users').value() });
